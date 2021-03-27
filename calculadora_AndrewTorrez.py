fe7f665fad0b16c7ffcfb53f94fbf7d5ed0b8d27
Unknown <cevs12321@gmail.com>
2021-03-27 19:40:34 -0400

    refactorizacion

--- calculadora_AndrewTorrez.py
+++ calculadora_AndrewTorrez.py
@@ -1,59 +1,59 @@
 #Andrew Torrez P
 #refactorizando
-while True:
-    o = 0
+def menu() :
     print("""
     ******** caluladora  ***********
     1.- Sumar numeros
     2.- Restar números
     3.- Multiplicar números
-    4.- dividar
+    4.- division
     5.- salir
     """)
-    o = int(input("Ingrese su opcion: ") )     
 
-    if o == 1:
-        c = int(input("cuantos numeros va a sumar? ") ) 
-        s=0
-        for v in range(c):
-            if v==0:
-                s=s+int(input("numero : ") ) 
-            else:
-                print("resultado parcial: ",s)
-                s=s+int(input("mas : ") ) 
-        print("las suma total es",s)
-    if o ==2:
-        c = int(input("cuantos numeros va a restar? ") ) 
-        r=0
-        for v in range(c):
-            if v==0:
-                r=int(input("numero : ") ) 
-            else:
-                print("resultado parcial: ",r)
-                r=r-int(input("menos : ") )
-        print("las resta total es",r)
-    if o ==3:
-        c = int(input("cuantos numeros va a multiplicar? ") ) 
-        m=1
-        for v in range(c):
-            if v==0:
-                m=m*int(input("numero : ") ) 
-            else:
-                print("resultado parcial: ",m)
-                m=m*int(input("por : ") )
-        print("el resultado total es",m)
-    if o ==4:
-        c = int(input("cuantos numeros va a dicidir? ") ) 
-        d=1
-        for v in range(c):
-            if v==0:
-                d=int(input("numero : ") ) 
-            else:
-                print("resultado parcial: ",d)
-                d=d/int(input("dividido entre : ") )
-        print("el resultado total es",d)
+def suma(valor1, valor2) :
+    return valor1 + valor2
+
+def resta(valor1, valor2) :
+    return valor1 - valor2
+
+def multiplicacion(valor1, valor2) :
+    return valor1 * valor2
+
+def division(valor1, valor2) :
+    return valor1/valor2
+
+
+while True:
+    menu()
+    o = int(input("Ingrese su opcion: ")) 
     if o==5 :
         print("hasta luego...!!!")
         break
+    c = int(input("cuantos numeros va a calcular? ") )    
+    if o == 1:
+        for v in range(c):
+            if v==0:
+                respuesta=suma(0,int(input("numero : ")))
+            else:
+                respuesta=suma(respuesta,int(input("numero : ")))
+    if o ==2:
+        for v in range(c):
+            if v==0:
+                respuesta=resta(int(input("numero : ")),0) 
+            else:
+                respuesta=resta(respuesta,int(input("numero : ")))
+    if o ==3:
+        for v in range(c):
+            if v==0:
+                respuesta=multiplicacion(1,int(input("numero : ")))
+            else:
+                respuesta=multiplicacion(respuesta,int(input("numero : ")))
+    if o ==4:
+        for v in range(c):
+            if v==0:
+                respuesta=division(int(input("numero : ")),1)
+            else:
+                respuesta=division(respuesta,int(input("numero : ")))
+    print("las resultado es", respuesta)
     if  o !=1 and o !=2 and o !=3 and o !=4 and o !=5:
         print ("opcion invalida") 
